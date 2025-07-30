ec8fd9a6aefc827b334f4a436ef45594237bf55b
GCHANTRE <78544473+GUILLE-bit@users.noreply.github.com>
2025-07-30 11:55:59 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -60,7 +60,7 @@ st.title("Predicción de Emergencia Agrícola con ANN")
 st.sidebar.header("Configuración")
 umbral_usuario = st.sidebar.number_input(
     "Umbral de EMEAC para 100%",
-    min_value=1.00,
+    min_value=0.5,
     max_value=3.00,
     value=2.77,
     step=0.01,
